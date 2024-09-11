1bd2321023af495e50251c561566260ee36e293f
Gleb <yastrebov0704@gmail.com>
2024-09-11 22:07:22 +0300

    Добавьте файлы проекта.

---
+++ dz.c
@@ -0,0 +1,37 @@
+#include <locale.h>
+#include <stdio.h> 
+
+void main() {
+	setlocale(LC_ALL, "RUS");
+	printf("              /\\ \n");
+	printf("             /  \\ \n");
+	printf("            /    \\ \n");
+	printf("           /      \\ \n");
+	printf("          /________\\ \n");
+	printf("         |          | \n");
+	printf("         |          | \n");
+	printf("         |    __    | \n");
+	printf("         |   |  |   | \n");
+	printf("         |   |__|   | \n");
+	printf("         |          | \n");
+	printf("         |          | \n");
+	printf("         |__________| \n");
+	printf("        /__/|____|\\__\\ \n");
+	printf("       /__/_|____|_\\__\\ \n");
+	printf("      /__/__|____|__\\__\\ \n");
+	printf("         |__|    |__|\ \n");
+	printf("          **      ** \n");
+	printf("         ****    ****\n");
+	printf("        ******  ****** \n");
+	printf("       **************** \n");
+	printf("      ****************** \n");
+	printf("     ––––––––––––––––––––\n");
+	printf("    ¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿ \n");
+	printf("                            \n");
+	printf("  ≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈ \n");
+	printf(" ““““““““““““““““““““““““““““ \n");
+	printf("¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿ \n");
+
+	return 0;
+}
+
